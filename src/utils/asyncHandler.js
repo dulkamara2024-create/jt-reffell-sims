9c1f220169f45c435e3025d909a9c70c95a3e708
Samuel A.Kamara <dulkamara2024@gmail.com>
2026-07-10 20:26:32 +0000

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,1 +1,5 @@
-
+// Wraps an async route handler so thrown errors/rejected promises reach
+// Express's error handler instead of crashing the process.
+module.exports = function asyncHandler(fn) {
+  return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+};
